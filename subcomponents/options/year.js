8b25064dde759120f899bb1f5ce4d75cb342371b
Albert van der Merwe <albertusvdmerwe@gmail.com>
2018-11-13 17:21:48 +0200

    year.tsx - tslint, type annotation

--- subcomponents/options/year.js
+++ subcomponents/options/year.js
@@ -11,29 +11,41 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
 import Selections from "./item/selections";
-var year = moment().year();
+var currentYear = moment().year();
 var Year = /** @class */ (function (_super) {
     __extends(Year, _super);
-    function Year() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function Year(props) {
+        var _this = _super.call(this, props) || this;
+        _this.handleOnClick = _this.handleOnClick.bind(_this);
+        return _this;
     }
     Year.prototype.getYearArray = function () {
         var yearItems = [];
-        for (var x = 1980; x <= year; x++) {
+        for (var x = currentYear - 15; x <= currentYear; x++) {
             yearItems.push(x);
         }
         return yearItems.reverse();
     };
+    Year.prototype.handleOnClick = function (value) {
+        var onChange = this.props.onChange;
+        onChange(value);
+    };
+    Year.prototype.getYearAsString = function (year) {
+        return year.toString();
+    };
     Year.prototype.render = function () {
-        var _a = this.props, onChange = _a.onChange, year = _a.year, month = _a.month, day = _a.day, headerStyles = _a.headerStyles, bodyStyles = _a.bodyStyles;
+        var _this = this;
+        var _a = this.props, year = _a.year, month = _a.month, day = _a.day, headerStyles = _a.headerStyles, bodyStyles = _a.bodyStyles;
         if (!year && !month && !day) {
             return (React.createElement("div", { className: "simple-react-date-selector-scrollables" },
                 React.createElement("div", { className: "simple-react-date-selector-selection-heading", style: headerStyles }, "Year"),
-                React.createElement("div", { style: bodyStyles }, this.getYearArray()
-                    .map(function (year) { return React.createElement(Selections, { key: "years-selector-" + year, value: String(year), onClick: function (value) { return onChange(value); } }); }))));
+                React.createElement("div", { style: bodyStyles }, this.getYearArray().map(function (yearValue) {
+                    var yearAsString = _this.getYearAsString(yearValue);
+                    return (React.createElement(Selections, { key: "years-selector-" + yearAsString, value: yearAsString, onClick: _this.handleOnClick }));
+                }))));
         }
         return null;
     };
--- subcomponents/options/year.tsx
+++ subcomponents/options/year.tsx
@@ -1,54 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
 
 import Selections from "./item/selections";
-const year = moment().year();
+const currentYear = moment().year();
 
-interface Props{
-    onChange:(value:string)=>void;
-    year:string;
-    month:string;
-    day:string;
-    headerStyles:object;
-    bodyStyles:object;
+interface Props {
+  onChange: (value: string) => void;
+  year: string;
+  month: string;
+  day: string;
+  headerStyles: object;
+  bodyStyles: object;
 }
 
-class Year extends Component <Props,{}>{
-    
-    getYearArray(){
-        var yearItems=[];
-        
-        for(var x=1980;x<=year;x++)
-        {
-            yearItems.push(x);
-        }
+class Year extends Component<Props, {}> {
+  constructor(props: Props) {
+    super(props);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-        return yearItems.reverse();
+  getYearArray(): number[] {
+    const yearItems = [];
+
+    for (let x = currentYear - 15; x <= currentYear; x++) {
+      yearItems.push(x);
     }
-    
-    render() {
 
-        const {onChange,year,month,day,headerStyles,bodyStyles}=this.props;
+    return yearItems.reverse();
+  }
 
-        if(!year&&!month&&!day)
-        {
-            return (
-                <div className="simple-react-date-selector-scrollables">
-                    <div className="simple-react-date-selector-selection-heading" style={headerStyles}>Year</div>
-                    <div style={bodyStyles}>
-                    {   
-                        this.getYearArray()
-                         .map(
-                            year=><Selections key={`years-selector-${year}`} value={String(year)} onClick={value=>onChange(value)}/>
-                        )
-                    }
-                    </div>
-                </div>
-            );
-        }
+  handleOnClick(value: string): void {
+    const { onChange } = this.props;
+    onChange(value);
+  }
 
-        return null;
+  getYearAsString(year: number): string {
+    return year.toString();
+  }
+
+  render() {
+    const { year, month, day, headerStyles, bodyStyles } = this.props;
+
+    if (!year && !month && !day) {
+      return (
+        <div className="simple-react-date-selector-scrollables">
+          <div
+            className="simple-react-date-selector-selection-heading"
+            style={headerStyles}
+          >
+            Year
+          </div>
+          <div style={bodyStyles}>
+            {this.getYearArray().map((yearValue: number) => {
+              const yearAsString: string = this.getYearAsString(yearValue);
+
+              return (
+                <Selections
+                  key={`years-selector-${yearAsString}`}
+                  value={yearAsString}
+                  onClick={this.handleOnClick}
+                />
+              );
+            })}
+          </div>
+        </div>
+      );
     }
+
+    return null;
+  }
 }
 
 export default Year;

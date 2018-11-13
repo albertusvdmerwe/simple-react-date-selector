c055ea4ce25abdd7551825d1c1a48532aaa3514a
Albert van der Merwe <albertusvdmerwe@gmail.com>
2018-11-13 17:42:03 +0200

    selected-dates-display.tsx - prettified

--- subcomponents/selected-dates-display.tsx
+++ subcomponents/selected-dates-display.tsx
@@ -1,43 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-interface Props{
-    year:string;
-    month:string;
-    day:string;
-    footerStyles:object;
+interface Props {
+  year: string;
+  month: string;
+  day: string;
+  footerStyles: object;
 }
 
-class SelectedDatesDisplay extends Component<Props,{}> {
-    
-    render() {
+class SelectedDatesDisplay extends Component<Props, {}> {
+  render() {
+    const { year, month, day, footerStyles } = this.props;
 
-        const {year,month,day,footerStyles}=this.props; 
-
-        if(year!=="")
-        {
-            return (
-                <div className="simple-react-date-selector-selections-made-container" style={footerStyles}>
-                     <table className="simple-react-date-selector-selections-made">
-                        <tbody>
-                            <tr>
-                                <td>
-                                    {year}
-                                </td>
-                                <td>
-                                    {month}
-                                </td>
-                                <td>
-                                    {day}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            );
-        }
-
-        return null;
+    if (year !== "") {
+      return (
+        <div
+          className="simple-react-date-selector-selections-made-container"
+          style={footerStyles}
+        >
+          <table className="simple-react-date-selector-selections-made">
+            <tbody>
+              <tr>
+                <td>{year}</td>
+                <td>{month}</td>
+                <td>{day}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
     }
+
+    return null;
+  }
 }
 
 export default SelectedDatesDisplay;

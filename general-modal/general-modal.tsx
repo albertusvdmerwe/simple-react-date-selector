630b599f4a807b509dce15f8339bc78c71e19ce7
Albert van der Merwe <albertusvdmerwe@gmail.com>
2018-11-14 15:06:15 +0200

    General modal - removed event listeners (onOpened and onClosed)

--- general-modal/general-modal.tsx
+++ general-modal/general-modal.tsx
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 interface Props {
   visible: boolean;
   children: JSX.Element;
-  onClick?: () => void;
-  onOpened: () => void;
-  onClosed: () => void;
+  onClick?: () => void
 }
 
 export default class GeneralModal extends Component<Props, {}> {
@@ -22,22 +20,10 @@ export default class GeneralModal extends Component<Props, {}> {
     }
   }
 
-  handleVisibilityEventListeners(): void {
-    const { visible, onOpened, onClosed } = this.props;
-
-    if (visible === true && typeof onOpened === "function") {
-      onOpened();
-    }
-
-    if (visible === false && typeof onClosed === "function") {
-      onClosed();
-    }
-  }
-
   render() {
     const { visible, children } = this.props;
     const additionalClass: string = visible ? "modal-visible" : "";
-    this.handleVisibilityEventListeners();
+    
 
     return (
       <div
